34d47b1e2dadbf95b70adc8e114399b689f6d322
PolRodriguez-CTS <prodriguez@ctsformacion.com>
2025-10-01 11:00:52 +0200

    barra de carga

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,12 +3,14 @@ using Unity.Burst;
 using Unity.Loading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
     public static SceneLoader instance;
 
     [SerializeField] private GameObject _loadingCanvas;
+    [SerializeField] private Image _loadingBar;
 
     void Awake()
     {
@@ -35,12 +37,27 @@ public class SceneLoader : MonoBehaviour
         _loadingCanvas.SetActive(true);
 
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        asyncLoad.allowSceneActivation = false;
+
+        float fakeLoadPercentage = 0;
 
         while (!asyncLoad.isDone)
         {
+            //_loadingBar.fillAmount = asyncLoad.progress; //carga según el progreso de carga, en escenas poco pesadas es casi instantaneo
 
+            //barra de carga falsa
+            fakeLoadPercentage += 0.01f;
+            _loadingBar.fillAmount = fakeLoadPercentage;
+
+            if (asyncLoad.progress >= 0.9f && fakeLoadPercentage >= 0.99f)
+            {
+                asyncLoad.allowSceneActivation = true;
+            }
+
+            float _loadingBarFakeTime = 0.01f;
+            yield return new WaitForSecondsRealtime(_loadingBarFakeTime);
         }
-        
+
         _loadingCanvas.SetActive(false);
     }
 }
